12c8384a7a9a7ff251998fa499a1e23ac0cff988
subeeshcbabu <subeeshcbabu@yahoo.co.in>
2016-02-16 09:11:10 -0800

    Set the 405 status code

--- petstore-full-express/handlers/pets.js
+++ petstore-full-express/handlers/pets.js
@@ -4,15 +4,15 @@
  */
 module.exports = {
     /**
-     * 
+     *
      * parameters: body
      * produces: application/json, application/xml
      */
     post: function addPet(req, res) {
-        res.sendStatus(501);
+        res.sendStatus(405);
     },
     /**
-     * 
+     *
      * parameters: body
      * produces: application/json, application/xml
      */
--- petstore-full-express/tests/test_pets.js
+++ petstore-full-express/tests/test_pets.js
@@ -12,8 +12,7 @@ var test = require('tape'),
 test('api', function (t) {
     var app = express();
 
-    
-    app.use(require('body-parser')());
+
     app.use(require('body-parser')());
 
     app.use(swaggerize({
@@ -21,38 +20,38 @@ test('api', function (t) {
         handlers: path.join(__dirname, '../handlers')
     }));
 
-    
+
     t.test('test post /pets', function (t) {
-        
+
         var body = {
             'name': "helloworld"
         };
-        
+
 
         request(app).post('/v2/pets').send(body)
         .end(function (err, res) {
             t.ok(!err, 'post /pets no error.');
-            
+
             t.strictEqual(res.statusCode, 405, 'post /pets 405 status.');
             t.end();
         });
     });
-    
+
     t.test('test put /pets', function (t) {
-        
+
         var body = {
             'name': "helloworld"
         };
-        
+
 
         request(app).put('/v2/pets').send(body)
         .end(function (err, res) {
             t.ok(!err, 'put /pets no error.');
-            
+
             t.strictEqual(res.statusCode, 400, 'put /pets 400 status.');
             t.end();
         });
     });
-    
+
 
 });
